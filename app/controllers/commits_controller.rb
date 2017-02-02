ca7b2ecf1deea3c29750798f799f3d7e7fd9a021
hanssens <github@hanssens.com>
2017-02-02 20:22:34 +0100

    List all (public) repo’s for the current user

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -1,18 +1,34 @@
 class CommitsController < ApplicationController
-  
+
   def index
 
     # redirect user to login if not authorized
-    if (!session[:token].present?)
-      redirect_to :controller => 'home', :action => 'index'
+    token = session[:token]
+    if (!token.present?)
+      redirect_to :controller => 'auth', :action => 'login'
     end
 
-    @token = session[:token]
-    client = Octokit::Client.new(:access_token => @token)
-
-    user = client.user
-    @foo = user.login
     
+    @client = Octokit::Client.new(:access_token => token)
+    @user = @client.user
+
+    # fetch repositories that are accessible to the authenticated user.
+    # this includes repositories owned by the authenticated user, 
+    # repositories where the authenticated user is a collaborator, 
+    # and repositories that the authenticated user has access to through 
+    # an organization membership.
+    
+    # TODO: client.user.repositories
+
+    # TODO: for each repository, get the commits for that user
+    
+    # docs: https://developer.github.com/v3/repos/#response
+    @repos = @client.repositories(@user)
+
+
+    # docs: https://developer.github.com/v3/repos/commits/#list-commits-on-a-repository
+    #@repositories.each do |repo|
+    first = @repos[0]
   end
 
 end
